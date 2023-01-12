aae2f17e93b3062beafc4a8873a49332071c5cb3
MikeChild <mcechild@gmail.com>
2023-01-12 13:17:31 -0800

    prep03

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is the magic number? ");
+        // string userInput = Console.ReadLine();
+        // int num = int.Parse(userInput);
+        Random randomGenerator = new Random();
+        int num = randomGenerator.Next(1, 100);
+
+        int guess = 0;
+
+        while (guess != num)
+        {
+            if (guess >= num){
+                Console.WriteLine("Lower");
+            }
+            else if (guess <= num){
+                Console.WriteLine("Higher");
+            }
+        }
+
+        Console.WriteLine("You guessed it!");
+
     }
 }
